3af035f8e378a323244cc88856ecc64f17a21e17
elex <elextraza@gmail.com>
2016-03-30 12:10:56 +0300

    login page

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,35 +1,4 @@
 
-<div class="w-container">
-  <h1 class="body-h1"><?php echo lang('login_heading');?>&nbsp;
-    <span class="body-h1-span">(<?php echo lang('login_subheading');?>)</span>
-
-  </h1>
-</div>
-<div class="w-container body-container login-container">
-  <div class="w-form login-form-container">
-    <?php echo form_open("auth/login");?>
-
-
-    <?php echo lang('login_identity_label', 'identity');?>
-    <?php echo form_input($identity);?>
-
-    <?php echo lang('login_password_label', 'password');?>
-    <?php echo form_input($password);?>
-
-    <?php echo lang('login_remember_label', 'remember');?>
-
-
-    <input style='width: 15px;height: 15px;' type="checkbox" name="remember" value="1" id="remember">
-    <br>
-    <?php echo form_submit('submit', lang('login_submit_btn'));?>
-
-
-    <?php echo form_close();?>
-    <br>
-    <a href="forgot_password"><?php echo lang('login_forgot_password');?></a>
-  </div>
-</div>
-
 
 
 <!-- BACKGROUND IMAGE -->
@@ -43,7 +12,7 @@
   <div class="cls-brand">
     <a class="box-inline" href="index.html">
       <!-- <img alt="Nifty Admin" src="img/logo.png" class="brand-icon"> -->
-      <span class="brand-title">Система учета заявок</span>
+      <span class="brand-title"><?php echo lang('login_heading');?></span>
     </a>
   </div>
 </div>
