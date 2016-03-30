36c16428a759f4409bbb3b4b82de96d4795a8301
elex <elextraza@gmail.com>
2016-03-30 11:33:09 +0300

    nf head

---
+++ application/views/nf/nf_head.php
@@ -0,0 +1,110 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard | Nifty - Responsive admin template.</title>
+
+
+    <!--STYLESHEET-->
+    <!--=================================================-->
+
+
+
+    <!--Bootstrap Stylesheet [ REQUIRED ]-->
+    <link href="/css/nf/bootstrap.min.css" rel="stylesheet">
+
+
+    <!--Nifty Stylesheet [ REQUIRED ]-->
+    <link href="/css/nf/nifty.min.css" rel="stylesheet">
+
+
+    <!--Font Awesome [ OPTIONAL ]-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+
+
+    <!--Switchery [ OPTIONAL ]-->
+    <link href="/css/nf/switchery.min.css" rel="stylesheet">
+
+
+    <!--Bootstrap Select [ OPTIONAL ]-->
+    <link href="/css/nf/bootstrap-select.min.css" rel="stylesheet">
+
+
+    <!--Bootstrap Tags Input [ OPTIONAL ]-->
+    <link href="/css/nf/bootstrap-tagsinput.css" rel="stylesheet">
+
+
+    <!--Chosen [ OPTIONAL ]-->
+    <link href="/css/nf/chosen.min.css" rel="stylesheet">
+
+
+    <!--noUiSlider [ OPTIONAL ]-->
+    <link href="/css/nf/jquery.nouislider.min.css" rel="stylesheet">
+    <link href="/css/nf/jquery.nouislider.pips.min.css" rel="stylesheet">
+
+
+    <!--Bootstrap Timepicker [ OPTIONAL ]-->
+    <link href="/css/nf/bootstrap-timepicker.min.css" rel="stylesheet">
+
+
+    <!--Bootstrap Datepicker [ OPTIONAL ]-->
+    <link href="/css/nf/bootstrap-datepicker.css" rel="stylesheet">
+
+
+    <!--Dropzone [ OPTIONAL ]-->
+    <link href="/css/nf/dropzone.css" rel="stylesheet">
+
+
+    <!--Summernote [ OPTIONAL ]-->
+    <link href="/css/nf/summernote.min.css" rel="stylesheet">
+
+
+    <!--Demo [ DEMONSTRATION ]-->
+    <link href="/css/nf/nifty-demo.min.css" rel="stylesheet">
+
+
+
+
+    <!--SCRIPT-->
+    <!--=================================================-->
+
+    <!--Page Load Progress Bar [ OPTIONAL ]-->
+    <link href="/css/nf/pace.min.css" rel="stylesheet">
+    <script src="/js/nf/pace.min.js"></script>
+
+
+
+    <!--
+
+    REQUIRED
+    You must include this in your project.
+
+    RECOMMENDED
+    This category must be included but you may modify which plugins or components which should be included in your project.
+
+    OPTIONAL
+    Optional plugins. You may choose whether to include it in your project or not.
+
+    DEMONSTRATION
+    This is to be removed, used for demonstration purposes only. This category must not be included in your project.
+
+    SAMPLE
+    Some script samples which explain how to initialize plugins or components. This category should not be included in your project.
+
+
+    Detailed information and more samples can be found in the document.
+
+    -->
+
+
+</head>
+
+<!--TIPS-->
+<!--You may remove all ID or Class names which contain "demo-", they are only used for demonstration. -->
+
+<body>
+<div id="container" class="effect mainnav-lg">
