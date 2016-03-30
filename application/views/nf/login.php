56b236830f9b6057d1f57279a10865e1691eca84
elex <elextraza@gmail.com>
2016-03-30 11:40:37 +0300

    login page

--- application/views/nf/login.php
+++ application/views/nf/login.php
@@ -10,7 +10,7 @@
         <div class="cls-brand">
             <a class="box-inline" href="index.html">
                 <!-- <img alt="Nifty Admin" src="img/logo.png" class="brand-icon"> -->
-                <span class="brand-title">Nifty <span class="text-thin">Admin</span></span>
+                <span class="brand-title">Система учета заявок <span class="text-thin">Вход</span></span>
             </a>
         </div>
     </div>
@@ -22,42 +22,42 @@
     <div class="cls-content">
         <div class="cls-content-sm panel">
             <div class="panel-body">
-                <p class="pad-btm">Sign In to your account</p>
+                <p class="pad-btm">Войдите в свой акаунт</p>
                 <form action="index.html">
                     <div class="form-group">
                         <div class="input-group">
                             <div class="input-group-addon"><i class="fa fa-user"></i></div>
-                            <input type="text" class="form-control" placeholder="Username">
+                            <input type="text" class="form-control" placeholder="Ваш логин">
                         </div>
                     </div>
                     <div class="form-group">
                         <div class="input-group">
                             <div class="input-group-addon"><i class="fa fa-asterisk"></i></div>
-                            <input type="password" class="form-control" placeholder="Password">
+                            <input type="password" class="form-control" placeholder="Ваш пароль">
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-xs-8 text-left checkbox">
                             <label class="form-checkbox form-icon">
-                                <input type="checkbox"> Remember me
+                                <input type="checkbox"> Запомнить меня
                             </label>
                         </div>
                         <div class="col-xs-4">
                             <div class="form-group text-right">
-                                <button class="btn btn-success text-uppercase" type="submit">Sign In</button>
+                                <button class="btn btn-success text-uppercase" type="submit">Войти</button>
                             </div>
                         </div>
                     </div>
-                    <div class="mar-btm"><em>- or -</em></div>
+                    <div class="mar-btm"><em>- или -</em></div>
                     <button class="btn btn-primary btn-lg btn-block" type="button">
-                        <i class="fa fa-facebook fa-fw"></i> Login with Facebook
+                        Подать заявку на использование
                     </button>
                 </form>
             </div>
         </div>
         <div class="pad-ver">
-            <a href="pages-password-reminder.html" class="btn-link mar-rgt">Forgot password ?</a>
-            <a href="pages-register.html" class="btn-link mar-lft">Create a new account</a>
+            <a href="pages-password-reminder.html" class="btn-link mar-rgt">Условия использования</a>
+            <a href="pages-register.html" class="btn-link mar-lft">Политика конфиденциальности</a>
         </div>
     </div>
     <!--===================================================-->
