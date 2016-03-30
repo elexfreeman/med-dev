e8bfa9bb83ebff3d2ab8c4caebddbe9dd44ec1eb
elex <elextraza@gmail.com>
2016-03-30 11:31:43 +0300

    nf footer

---
+++ application/views/nf/nf_footer.php
@@ -0,0 +1,155 @@
+
+
+<!-- FOOTER -->
+<!--===================================================-->
+<footer id="footer">
+
+    <!-- Visible when footer positions are fixed -->
+    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+    <div class="show-fixed pull-right">
+        <ul class="footer-list list-inline">
+            <li>
+                <p class="text-sm">SEO Proggres</p>
+                <div class="progress progress-sm progress-light-base">
+                    <div style="width: 80%" class="progress-bar progress-bar-danger"></div>
+                </div>
+            </li>
+
+            <li>
+                <p class="text-sm">Online Tutorial</p>
+                <div class="progress progress-sm progress-light-base">
+                    <div style="width: 80%" class="progress-bar progress-bar-primary"></div>
+                </div>
+            </li>
+            <li>
+                <button class="btn btn-sm btn-dark btn-active-success">Checkout</button>
+            </li>
+        </ul>
+    </div>
+
+
+
+    <!-- Visible when footer positions are static -->
+    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+    <div class="hide-fixed pull-right pad-rgt">Currently v2.3</div>
+
+
+
+    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+    <!-- Remove the class name "show-fixed" and "hide-fixed" to make the content always appears. -->
+    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+    <p class="pad-lft">&#0169; 2015 Your Company</p>
+
+
+
+</footer>
+<!--===================================================-->
+<!-- END FOOTER -->
+
+
+<!-- SCROLL TOP BUTTON -->
+<!--===================================================-->
+<button id="scroll-top" class="btn"><i class="fa fa-chevron-up"></i></button>
+<!--===================================================-->
+
+
+
+</div>
+<!--===================================================-->
+<!-- END OF CONTAINER -->
+
+
+
+
+
+
+<!--JAVASCRIPT-->
+<!--=================================================-->
+
+<!--jQuery [ REQUIRED ]-->
+<script src="/js/nf/jquery-2.1.1.min.js"></script>
+
+
+<!--BootstrapJS [ RECOMMENDED ]-->
+<script src="/js/nf/bootstrap.min.js"></script>
+
+
+<!--Fast Click [ OPTIONAL ]-->
+<script src="/js/nf/fastclick.min.js"></script>
+
+
+<!--Nifty Admin [ RECOMMENDED ]-->
+<script src="/js/nf/nifty.min.js"></script>
+
+
+<!--Switchery [ OPTIONAL ]-->
+<script src="/js/nf/switchery.min.js"></script>
+
+
+<!--Bootstrap Select [ OPTIONAL ]-->
+<script src="/js/nf/bootstrap-select.min.js"></script>
+
+
+<!--Bootstrap Tags Input [ OPTIONAL ]-->
+<script src="/js/nf/bootstrap-tagsinput.min.js"></script>
+
+
+<!--Chosen [ OPTIONAL ]-->
+<script src="/js/nf/chosen.jquery.min.js"></script>
+
+
+<!--noUiSlider [ OPTIONAL ]-->
+<script src="/js/nf/jquery.nouislider.all.min.js"></script>
+
+
+<!--Bootstrap Timepicker [ OPTIONAL ]-->
+<script src="/js/nf/bootstrap-timepicker.min.js"></script>
+
+
+<!--Bootstrap Datepicker [ OPTIONAL ]-->
+<script src="/js/nf/bootstrap-datepicker.js"></script>
+
+
+<!--Dropzone [ OPTIONAL ]-->
+<script src="/js/nf/dropzone.min.js"></script>
+
+
+<!--Summernote [ OPTIONAL ]-->
+<script src="/js/nf/summernote.min.js"></script>
+
+
+<!--Demo script [ DEMONSTRATION ]-->
+<script src="/js/nf/nifty-demo.min.js"></script>
+
+
+<!--Form Component [ SAMPLE ]-->
+<script src="/js/nf/form-component.js"></script>
+
+
+
+<!--
+
+REQUIRED
+You must include this in your project.
+
+RECOMMENDED
+This category must be included but you may modify which plugins or components which should be included in your project.
+
+OPTIONAL
+Optional plugins. You may choose whether to include it in your project or not.
+
+DEMONSTRATION
+This is to be removed, used for demonstration purposes only. This category must not be included in your project.
+
+SAMPLE
+Some script samples which explain how to initialize plugins or components. This category should not be included in your project.
+
+
+Detailed information and more samples can be found in the document.
+
+-->
+
+
+</body>
+</html>
