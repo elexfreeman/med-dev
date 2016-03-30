a2ac0ac6f2a9367bda1cbf5948a141f06bf3fb8a
elex <elextraza@gmail.com>
2016-03-30 16:39:23 +0300

    patients add

--- application/views/patients/add.php
+++ application/views/patients/add.php
@@ -105,8 +105,155 @@
                                 <h3 class="panel-title">Полный адрес:</strong> 612961, Самара ул. Садовая д. 78, кв 4 </h3>
                             </div>
 
+                            <div class="panel-body">
+                                <div class="row">
+                                    <div class="col-sm-4">
+                                        <div class="form-group">
+                                            <label class="control-label">Почтовый индекс</label>
+                                            <input type="text" name="POST_INDEX" value="<?php echo set_value('surname'); ?>"  class="form-control <?php if(form_error('surname')!='') echo 'has-error'; ?>" name="surname">
+
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <div class="form-group">
+                                            <label class="control-label">Регион</label>
+                                            <!-- Default choosen -->
+                                            <!--===================================================-->
+                                            <select data-placeholder="Выберите из списка" id="demo-chosen-select" tabindex="2" class="form-control">
+                                                <option value="">Выберите из списка...</option>
+                                                <option value="First">First Choice</option>
+                                                <option value="Second">Second Choice</option>
+                                                <option value="Third">Third Choice</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <div class="form-group">
+                                            <label class="control-label">Область</label>
+                                            <!-- Default choosen -->
+                                            <!--===================================================-->
+                                            <select data-placeholder="Выберите из списка" id="demo-chosen-select" tabindex="2" class="form-control">
+                                                <option value="">Выберите из списка...</option>
+                                                <option value="First">First Choice</option>
+                                                <option value="Second">Second Choice</option>
+                                                <option value="Third">Third Choice</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-4">
+                                        <div class="form-group">
+                                            <label class="control-label">Город</label>
+                                            <!-- Default choosen -->
+                                            <!--===================================================-->
+                                            <select data-placeholder="Выберите из списка" id="demo-chosen-select" tabindex="2" class="form-control">
+                                                <option value="">Выберите из списка...</option>
+                                                <option value="First">First Choice</option>
+                                                <option value="Second">Second Choice</option>
+                                                <option value="Third">Third Choice</option>
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm-4">
+                                        <div class="form-group">
+                                            <label class="control-label">Улица</label>
+                                            <!-- Default choosen -->
+                                            <!--===================================================-->
+                                            <select data-placeholder="Выберите из списка" id="demo-chosen-select" tabindex="2" class="form-control">
+                                                <option value="">Выберите из списка...</option>
+                                                <option value="First">First Choice</option>
+                                                <option value="Second">Second Choice</option>
+                                                <option value="Third">Third Choice</option>
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm-2">
+                                        <div class="form-group">
+                                            <label class="control-label">Корпус</label>
+                                            <input type="text" name="POST_INDEX" value="<?php echo set_value('surname'); ?>"  class="form-control <?php if(form_error('surname')!='') echo 'has-error'; ?>" name="surname">
+
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm-2">
+                                        <div class="form-group">
+                                            <label class="control-label">Дом</label>
+                                            <input type="text" name="POST_INDEX" value="<?php echo set_value('surname'); ?>"  class="form-control <?php if(form_error('surname')!='') echo 'has-error'; ?>" name="surname">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="panel">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Block styled form</h3>
+                            </div>
+
                             <!--Block Styled Form -->
                             <!--===================================================-->
+
+                            <div class="panel-body">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label class="control-label">Фамилия</label>
+                                            <input type="text" name="surname" value="<?php echo set_value('surname'); ?>"  class="form-control <?php if(form_error('surname')!='') echo 'has-error'; ?>" name="surname">
+
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label class="control-label">Имя</label>
+                                            <input type="text" value="<?php echo set_value('name'); ?>" class="form-control <?php if(form_error('name')!='') echo 'has-error'; ?>" name="name">
+
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label class="control-label">Отчество</label>
+                                            <input type="text"  value="<?php echo set_value('secname'); ?>" class="form-control <?php if(form_error('secname')!='') echo 'has-error'; ?>" name="secname">
+
+
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="form-group date">
+                                            <label class="control-label">Дата рождения</label>
+                                            <input type="text" class="form-control" name="birthday" >
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="radio">
+                                            <!-- Inline radio buttons -->
+                                            <label class="form-radio form-normal form-text"><input type="radio" checked name="sex"> Мужчина</label>
+                                            <label class="form-radio form-normal form-text"><input type="radio" name="sex"> Женщина</label>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <!--===================================================-->
+                            <!--End Block Styled Form -->
+
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="panel">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Полный адрес:</strong> 612961, Самара ул. Садовая д. 78, кв 4 </h3>
+                            </div>
 
                             <div class="panel-body">
                                 <div class="row">
@@ -188,15 +335,10 @@
                                         </div>
                                     </div>
                                 </div>
-
-                                </div>
-
-
-                            <!--===================================================-->
-                            <!--End Block Styled Form -->
-
+                            </div>
                         </div>
                     </div>
+
                 </div>
 
             </form>
