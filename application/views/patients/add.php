fd5b3ee54b97b7e6aaf8879db72ac146aed87569
elex <elextraza@gmail.com>
2016-03-30 15:06:51 +0300

    patients add

--- application/views/patients/add.php
+++ application/views/patients/add.php
@@ -1,3 +1,51 @@
+<div class="boxed">
+
+    <!--CONTENT CONTAINER-->
+    <!--===================================================-->
+    <div id="content-container">
+
+        <!--Page Title-->
+        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+        <div id="page-title">
+            <h1 class="page-header text-overflow">Пациенты</h1>
+
+            <!--Searchbox-->
+            <div class="searchbox">
+                <div class="input-group custom-search-form">
+                    <input type="text" class="form-control" placeholder="Search..">
+							<span class="input-group-btn">
+								<button class="text-muted" type="button"><i class="fa fa-search"></i></button>
+							</span>
+                </div>
+            </div>
+        </div>
+        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+        <!--End page title-->
+
+
+        <!--Breadcrumb-->
+        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+        <ol class="breadcrumb">
+            <li><a href="/">Рабочий стол</a></li>
+            <li><a href="/patients">Пациенты</a></li>
+            <li><a href="/patients/add">Добавить</a></li>
+        </ol>
+        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+        <!--End breadcrumb-->
+
+
+
+
+        <!--Page content-->
+        <!--===================================================-->
+        <div id="page-content">
+
+
+        </div>
+    </div>
+</div>
+
+
 
 <div class="w-container">
     <h1 class="body-h1">Пациент&nbsp;<span class="body-h1-span">(добавление)</span></h1>
