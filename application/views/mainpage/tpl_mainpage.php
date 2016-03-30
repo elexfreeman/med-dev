f254a4ae18e2f26e1927feba5cb3f4d692e87899
elex <elextraza@gmail.com>
2016-03-30 13:04:45 +0300

    main page

--- application/views/mainpage/tpl_mainpage.php
+++ application/views/mainpage/tpl_mainpage.php
@@ -14,6 +14,40 @@
     <!--===================================================-->
     <div id="content-container">
 
+        <!--Page Title-->
+        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+        <div id="page-title">
+            <h1 class="page-header text-overflow">Рабочий стол</h1>
+
+            <!--Searchbox-->
+            <div class="searchbox">
+                <div class="input-group custom-search-form">
+                    <input type="text" class="form-control" placeholder="Search..">
+							<span class="input-group-btn">
+								<button class="text-muted" type="button"><i class="fa fa-search"></i></button>
+							</span>
+                </div>
+            </div>
+        </div>
+        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+        <!--End page title-->
+
+
+        <!--Breadcrumb-->
+        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+        <ol class="breadcrumb">
+            <li><a href="/">Рабочий стол</a></li>
+
+        </ol>
+        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+        <!--End breadcrumb-->
+
+
+
+
+        <!--Page content-->
+        <!--===================================================-->
+        <div id="page-content">
         <div class="row">
             <div class="col-md-6 col-lg-3">
 
@@ -89,6 +123,7 @@
                 </div>
 
             </div>
+        </div>
         </div>
     </div>
 </div>
