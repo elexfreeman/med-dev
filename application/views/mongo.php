fdd7d6cb341582a16c13420ffd9b7f63299e7c86
elex <elextraza@gmail.com>
2016-03-31 18:45:38 +0300

    add mongoDB driver

--- application/views/mongo.php
+++ application/views/mongo.php
@@ -1,7 +1,41 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 31.03.2016
- * Time: 18:44
- */
+<div class="boxed">
+
+    <!--CONTENT CONTAINER-->
+    <!--===================================================-->
+    <div id="content-container">
+
+        <!--Page Title-->
+        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+        <div id="page-title">
+            <h1 class="page-header text-overflow">Рабочий стол</h1>
+
+            <!--Searchbox-->
+            <div class="searchbox">
+                <div class="input-group custom-search-form">
+                    <input type="text" class="form-control" placeholder="Поиск..">
+							<span class="input-group-btn">
+								<button class="text-muted" type="button"><i class="fa fa-search"></i></button>
+							</span>
+                </div>
+            </div>
+        </div>
+        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+        <!--End page title-->
+
+
+        <!--Breadcrumb-->
+        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+        <ol class="breadcrumb">
+            <li><a href="/">Рабочий стол</a></li>
+
+        </ol>
+        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+        <!--End breadcrumb-->
+
+
+        <!--Page content-->
+        <!--===================================================-->
+        <div id="page-content">
+        </div>
+    </div>
+</div>
